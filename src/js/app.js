1ed10ee1c4827c1ac5a71fe69ee09941f0b975d1
FidanAsadova55555 <fidanra@code.edu.az>
2024-10-04 08:08:23 +0400

    algorithms are added

---
+++ src/js/app.js
@@ -0,0 +1,37 @@
+
+let originalNumb = 121;
+let reversedNumb = 0;
+
+if (originalNumb < 0 || (originalNumb % 10 === 0 && originalNumb !== 0)) {
+    console.log(false); 
+} else {
+    let x = originalNumb; 
+    while (x > 0) {
+        let lastDigit = x % 10;  
+        reversedNumb = reversedNumb * 10 + lastDigit; 
+        x = Math.floor(x / 10);  
+    }
+
+    console.log(reversedNumb); }
+
+
+    // The second task-duplicates
+    function findDuplicate(nums) {
+    
+        let sNumbers = [];
+   for (let i = 0; i < nums.length; i++) {
+       
+            if (sNumbers.includes(nums[i])) {
+            
+                return true;
+            }
+            sNumbers.push(nums[i]);
+        }
+        return false;
+    }
+    
+ 
+    console.log(findDuplicate([1, 2, 3, 1])); 
+    console.log(findDuplicate([1, 2, 3, 4]));  
+    console.log(findDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); 
+    
