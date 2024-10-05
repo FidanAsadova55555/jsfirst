fa29385ba0dd1ed6bc1c128b5baf874cf00df021
FidanAsadova55555 <fidanra@code.edu.az>
2024-10-06 00:10:23 +0400

    added 2nd version

--- src/js/app.js
+++ src/js/app.js
@@ -34,4 +34,15 @@ if (originalNumb < 0 || (originalNumb % 10 === 0 && originalNumb !== 0)) {
     console.log(findDuplicate([1, 2, 3, 1])); 
     console.log(findDuplicate([1, 2, 3, 4]));  
     console.log(findDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); 
+// vtoroy sposob resheniya
+    function findDuplicate(nums) {
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                if (nums[i] === nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     
